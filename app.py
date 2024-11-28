e084a8a513ef441377dd4a5684a29e69f747ad5c
kerollosy <kerollos.em@gmail.com>
2024-11-28 18:42:52 +0200

    Add registration route and update links in templates

--- app.py
+++ app.py
@@ -8,5 +8,9 @@ app = Flask(__name__, static_url_path='/')
 def home():
     return render_template('index.html')
 
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
